949ccbb88be3387600bcf8199dd92dd623c97bd2
@Olvares <olivieradjagba@gmail.com>
2023-12-20 08:39:04 +0200

    Profile almost done

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,13 +1,29 @@
 "use strict";
-let profile = {
-  name: "Adjagba",
-  firstname: "Olivier Mahumawon",
-  fullname: `${this.firstname} ${this.name}`,
+let profileInfo = {
+  surname: "Adjagba",
+  firstname: "Olivier",
+  fullname: function () {
+    const { firstname, surname } = this;
+    return `${firstname} ${surname}`;
+  },
   email: "olivier@aims.ac.za",
   major: "Mathematical and Modeling Engineering",
   title: { short: "Eng.", long: "Engineer" },
   degree: "Mathematics and Modeling",
-  fulldegree: `${this.degree} ${this.title.long}`,
+  fulldegree: function () {
+    const { degree, title } = this;
+    return `${degree} ${title.long}`;
+  },
+  imgSrc: "assets/img/profile-img.HEIC",
+  social: [
+    { name: "LinkedIn", icon: "bx bxl-linkedin", class: "linkedin", url: "https://www.linkedin.com/in/olivieradjagba" },
+    { name: "Github", icon: "bx bxl-github", class: "github", url: "https://github.com/olivieradjagba" },
+    { name: "Twitter", icon: "bx bxl-twitter", class: "twitter", url: "https://twitter.com/olivier_adjagba" },
+  ],
+  phone: {
+    mobile: "+27 071 432 1807",
+    whatsapp: "+229 67 345 963",
+  },
   address: {
     country: "South Africa",
     city: "Cape Town",
@@ -15,6 +31,27 @@ let profile = {
     street: "06 Melrose Road",
     postalcode: "7945",
   },
+  website: "https://olivieradjagba.github.io",
+  getAbout: function () {
+    const { website } = this;
+    return [
+      { id: "website", name: "Website", class: "my-website" },
+      { id: "degree", name: "Degree", text: "Master" },
+      { id: "address", name: "Address", class: "my-address" },
+      { id: "phone", name: "Phone", class: "my-phone" },
+      { id: "email", name: "Email", class: "my-email" },
+      { id: "whatsapp", name: "Whatsapp", class: "my-whatsapp" },
+    ];
+  },
 };
 
-export { profile };
+// // Read the Excel file
+// const workbook = XLSX.readFile("assets/files/data.xlsx");
+
+// // Get the first sheet
+// const worksheet = workbook.Sheets[sheetName];
+
+// // Convert the sheet data to JSON format
+// const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+// console.log(jsonData);
