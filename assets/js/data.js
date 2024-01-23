b74b8b5f34ff28dd458b0ebaaa4cfef5da02b904
@Olvares <olivieradjagba@gmail.com>
2024-01-23 07:31:47 +0200

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,256 @@
+"use strict";
+
+const currentYear = new Date().getFullYear();
+
+const profileInfo = {
+  surname: "Adjagba",
+  firstname: "Olivier",
+  fullname: function () {
+    const { firstname, surname } = this;
+    return `${firstname} ${surname}`;
+  },
+  email: "olivier@aims.ac.za",
+  major: "Mathematical and Modeling Engineering",
+  title: { short: "Eng.", long: "Engineer" },
+  degree: "Mathematics and Modeling", // | AI/ML Master Student",
+  fulldegree: function () {
+    const { degree, title } = this;
+    return `${degree} ${title.long}`;
+  },
+  imgSrc: "assets/images/profile-img.png",
+  social: [
+    { id: "linkedin", name: "LinkedIn", icon: "logo-linkedin", url: "https://www.linkedin.com/in/olivieradjagba" },
+    { id: "github", name: "Github", icon: "logo-github", url: "https://github.com/olivieradjagba" },
+    { id: "twitter", name: "Twitter", icon: "logo-twitter", url: "https://twitter.com/olivier_adjagba" },
+  ],
+  phone: {
+    mobile: "+27 071 432 1807",
+    whatsapp: "+229 67 345 963",
+  },
+  address: {
+    country: "South Africa",
+    city: "Cape Town",
+    district: "Muizenberg",
+    street: "06 Melrose Road",
+    postalcode: "7945",
+  },
+  fulladdress: function (detailled = false) {
+    const { address } = this;
+    return detailled
+      ? `${address.street}, ${address.district}, ${address.postalcode} ${address.city}, ${address.country}`
+      : `${address.city}, ${address.country}`;
+  },
+  website: "olivieradjagba.github.io",
+  getAbout: function () {
+    const { email, phone, whatsapp, address, website } = this;
+    return [
+      { id: "website", name: "Website", class: "my-website", icon: "globe-outline" },
+      { id: "degree", name: "Degree", text: "Master", icon: "school-outline" },
+      { id: "address", name: "Address", class: "my-address", icon: "location-outline" },
+      { id: "phone", name: "Phone", class: "my-phone", icon: "phone-portrait-outline" },
+      { id: "email", name: "Email", class: "my-email", icon: "mail-outline" },
+      { id: "whatsapp", name: "Whatsapp", class: "my-whatsapp", icon: "logo-whatsapp" },
+    ];
+  },
+  getPhone: function () {
+    const { phone } = this;
+    return phone;
+  },
+};
+
+const universities = [
+  {
+    id: "aims",
+    name: "African Institute of Mathematical Sciences",
+    shortname: "AIMS",
+    city: "Cape Town",
+    country: "South Africa",
+  },
+  {
+    id: "unstim",
+    name: "University of Science, Technology, Engineering and Mathematics",
+    shortname: "UNSTIM",
+    city: "Abomey",
+    country: "Benin",
+  },
+];
+
+const resumeInfo = {
+  education: [
+    {
+      id: "aims",
+      university: universities.find((u) => u.id === "aims"),
+      degree: "Master of AI for Science",
+      grade: "",
+      year: { from: 2023, to: 2024 },
+      ongoing: true,
+      description:
+        "Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend",
+    },
+    {
+      id: "gmm",
+      university: universities.find((u) => u.id === "unstim"),
+      degree: "Engineering Degree",
+      grade: "15.42/20",
+      year: { from: 2018, to: 2021 },
+      description:
+        "Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend",
+    },
+    {
+      id: "prepa",
+      university: universities.find((u) => u.id === "unstim"),
+      degree: "Preparatory Classes",
+      grade: "74.85/100",
+      year: { from: 2016, to: 2018 },
+      description:
+        "Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend",
+    },
+  ],
+  experience: [
+    {
+      id: "cosit",
+      company: "COSIT Benin",
+      position: "Web Front-end developer",
+      year: { from: 2022, to: 2023 },
+      description: "Design and maintenance of a web platform.",
+      achievements: [
+        "Development of a SIG platform for UNTIM university",
+        "Development of woods traceability platform for ONAB",
+        "Development of cars repair management platform",
+        "Web platform monitoring and management",
+        "Interns follow-up",
+      ],
+      city: "Cotonou",
+      country: "Benin",
+    },
+    {
+      id: "gti",
+      company: "Green Tech Innovation",
+      position: "Intern",
+      year: { from: 2021, to: 2022 },
+      description: "Bio-statistics analysis and web development.",
+      achievements: [
+        "Data analysis on the molecular genetic characterisation of plantain accessions in Benin for a PhD student",
+        "Data analysis on master’s thesis topics",
+        "Contribution to the company’s website design",
+        "Design of a mini plant sales website with ReactJS and PHP REST API",
+        "Analysis of data collected for the « Global Trial on Agroecological Approches to Fall Armyworm Management » project",
+      ],
+      city: "Abomey-Calavi",
+      country: "Benin",
+    },
+    {
+      id: "dp",
+      company: "dP Divecosys in partnership with UNSTIM",
+      position: "Intern",
+      year: { from: 2021, to: 2021 },
+      description: "Data collection on fall armyworm.",
+      achievements: [
+        "Implementation of an experimental device on the agroecological management of the fall armyworm for the « Global Trial on Agroecological Approches to Fall Armyworm Management » project",
+        "Data collection on the fall armyworm (Spodoptera frugiperda) at 3, 6 and 9 months after planting as well as at harvest",
+        "Sending data on the dedicated platform using KoboColect",
+      ],
+      city: "Dassa",
+      country: "Benin",
+    },
+  ],
+  skills: [
+    { id: "python", name: "Python", level: 90 },
+    { id: "r", name: "R", level: 80 },
+    { id: "matlab", name: "MATLAB", level: 75 },
+    { id: "c/c++", name: "C/C++", level: 70 },
+    { id: "cuda", name: "CUDA", level: 60 },
+    { id: "html/css", name: "HTML/CSS", level: 95 },
+    { id: "js", name: "JavaScript", level: 90 },
+    { id: "sql", name: "SQL", level: 80 },
+    { id: "git", name: "Git", level: 80 },
+    { id: "vue", name: "VueJS", level: 85 },
+    { id: "react", name: "ReactJS", level: 75 },
+    { id: "php", name: "PHP", level: 70 },
+  ],
+  projects: [
+    {
+      id: "app1",
+      name: "App 1",
+      category: "app",
+      filter: "app",
+      cover: "assets/img/portfolio/portfolio-1.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "web3",
+      name: "Web 3",
+      category: "web",
+      filter: "web",
+      cover: "assets/img/portfolio/portfolio-2.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "app2",
+      name: "App 2",
+      category: "app",
+      filter: "app",
+      cover: "assets/img/portfolio/portfolio-3.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "card2",
+      name: "Card 2",
+      category: "card",
+      filter: "card",
+      cover: "assets/img/portfolio/portfolio-4.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "web2",
+      name: "Web 2",
+      category: "web",
+      filter: "web",
+      cover: "assets/img/portfolio/portfolio-5.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "app3",
+      name: "App 3",
+      category: "app",
+      filter: "app",
+      cover: "assets/img/portfolio/portfolio-6.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "card1",
+      name: "Card 1",
+      category: "card",
+      filter: "card",
+      cover: "assets/img/portfolio/portfolio-7.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "card3",
+      name: "Card 3",
+      category: "card",
+      filter: "card",
+      cover: "assets/img/portfolio/portfolio-8.jpg",
+      description: "SIGS is a platform for university students",
+    },
+    {
+      id: "web3",
+      name: "Web 3",
+      category: "web",
+      filter: "web",
+      cover: "assets/img/portfolio/portfolio-9.jpg",
+      description: "SIGS is a platform for university students",
+    },
+  ],
+};
+
+// // Read the Excel file
+// const workbook = XLSX.readFile("assets/files/data.xlsx");
+
+// // Get the first sheet
+// const worksheet = workbook.Sheets[sheetName];
+
+// // Convert the sheet data to JSON format
+// const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+// console.log(jsonData);
---
+++ assets/js/main.js
@@ -0,0 +1,272 @@
+/**
+ * Template Name: iPortfolio
+ * Updated: Nov 17 2023 with Bootstrap v5.3.2
+ * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
+  ("use strict");
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos,
+      behavior: "smooth",
+    });
+  };
+
+  //? -------- My own code to feed data on the website -------
+  /**
+   * Useful functions
+   */
+  const innerText = (els, text, props = null) => {
+    if (els) {
+      if (Array.isArray(els)) {
+        els.forEach((el) => {
+          el.innerText = text;
+          if (props)
+            props.forEach((prop) => {
+              el[prop.name] = prop.value;
+            });
+        });
+      } else {
+        els.innerText = text;
+        if (props)
+          props.forEach((prop) => {
+            els[prop.name] = prop.value;
+          });
+      }
+    }
+  };
+
+  const innerHTML = (els, html) => {
+    if (els) {
+      if (Array.isArray(els)) {
+        els.forEach((el) => {
+          el.innerHTML = html;
+        });
+      } else {
+        els.innerHTML = html;
+      }
+    }
+  };
+
+  const strCompare = (str1, str2) => !str1.localeCompare(str2, "en", { sensitivity: "base" });
+
+  /**
+   * Title
+   */
+  const title = `Olivier - Portfolio`;
+  innerText(select("title"), title);
+
+  /**
+   * Profile
+   */
+  (function () {
+    // Hero
+    innerText(select(".my-name", true), profileInfo.fullname());
+    // Degree
+    innerText(select(".my-degree", true), profileInfo.fulldegree());
+    // Profile image
+    const imgEls = select("img.my-profile", true);
+    if (imgEls)
+      imgEls.forEach((el) => {
+        el.src = profileInfo.imgSrc;
+        el.alt = profileInfo.fullname();
+      });
+
+    // Social links
+    const socials = select(".sidebar-info_more .social-list");
+    if (socials) {
+      let dataSocialInfo = socials.getAttribute("data-social-info").split(",");
+      let html = "";
+      dataSocialInfo.forEach((info) => {
+        info = info.trim();
+        let findSocial = profileInfo.social.find((item) => strCompare(item.id, info));
+        if (findSocial) {
+          html += `
+            <li class="social-item">
+              <a href="${findSocial.url}" class="social-link">
+                <ion-icon name="${findSocial.icon}"></ion-icon>
+              </a>
+            </li>`;
+        }
+      });
+      innerHTML(socials, html);
+    }
+    // About
+    const aboutEl = select(".sidebar-info_more .contacts-list");
+    if (aboutEl) {
+      const aboutInfo = profileInfo.getAbout();
+      let dataContactInfo = aboutEl.getAttribute("data-contact-info").split(",");
+      let html = "";
+      dataContactInfo.forEach((info) => {
+        info = info.trim();
+        let findInfo = aboutInfo.find((item) => strCompare(item.id, info));
+        if (findInfo) {
+          html += `
+            <li class="contact-item">
+              <div class="icon-box">
+                <ion-icon name="${findInfo.icon}"></ion-icon>
+              </div>
+              <div class="contact-info">
+                <p class="contact-title">${findInfo.name}</p>
+                <span class="${findInfo.class}">${findInfo.text || ""}</span>
+              </div>
+            </li>`;
+        }
+      });
+      innerHTML(aboutEl, html);
+    }
+    // Website
+    websiteEls = select(".my-website", true);
+    innerHTML(websiteEls, `<a href="https://${profileInfo.website}" class="contact-link" target="_blank">${profileInfo.website}</a>`);
+    // Email
+    const emailEls = select(".my-email", true);
+    innerHTML(emailEls, `<a href="mailto:${profileInfo.email}" class="contact-link" target="_blank">${profileInfo.email}</a>`);
+    // Phone
+    const phoneEls = select(".my-phone", true);
+    innerHTML(phoneEls, `<a href="tel:${profileInfo.phone.mobile}" class="contact-link" target="_blank">${profileInfo.phone.mobile}</a>`);
+    const whatsappEls = select(".my-whatsapp", true);
+    innerHTML(whatsappEls, `<a href="https://wa.link/4sg5dd" class="contact-link" target="_blank">${profileInfo.phone.whatsapp}</a>`);
+    // Address
+    const addressEls = select(".my-address", true);
+    innerHTML(addressEls, `<address>${profileInfo.fulladdress()}</address>`);
+    const locationEls = select(".my-location", true);
+    innerHTML(locationEls, `<address>${profileInfo.fulladdress(true)}</address>`);
+    // Media
+  })();
+
+  /**
+   * Resume
+   */
+  (function () {
+    // Education
+    const educationEl = select("#resume .education");
+    if (educationEl) {
+      const { education } = resumeInfo;
+      educationEl.innerHTML = "";
+      education.forEach((item) => {
+        educationEl.innerHTML += `
+        <div class="resume-item">
+          <h4>${item.degree}</h4>
+          <h5>${item.year.from} - ${((item.year.to < currentYear || !item.ongoing) && item.year.to) || "present"}</h5>
+          <p><em>${item.university.name}, ${item.university.city}, ${item.university.country}</em></p>
+          <p>${item.description}</p>
+        </div>`;
+      });
+    }
+    // Experience
+    const experienceEl = select("#resume .experience");
+    if (experienceEl) {
+      const { experience } = resumeInfo;
+      experienceEl.innerHTML = "";
+      experience.forEach((item) => {
+        experienceEl.innerHTML += `
+        <div class="resume-item">
+          <h4>${item.position}</h4>
+          <h5>${item.year.from} ${
+          !item.ongoing && item.year.from == item.year.to ? "" : "- " + (((item.year.to < currentYear || !item.ongoing) && item.year.to) || "present")
+        }</h5>
+          <p><em>${item.company}</em></p>
+          <p>${item.description}</p>
+          <ul>${(item.achievements.map((ach) => "<li>" + ach + "</li>") || []).join("")}</ul>
+          </ul>
+        </div>`;
+      });
+    }
+    // Skills
+    const skillsEls = select(".skills-content .my-skills", true);
+    if (skillsEls) {
+      const { skills } = resumeInfo;
+      skillsEls.forEach((el) => {
+        let dataSkills = el.getAttribute("data-skills").split(",");
+        let html = "";
+        dataSkills.forEach((skill) => {
+          skill = skill.trim();
+          let findSkill = skills.find((item) => item.id == skill);
+          if (findSkill) {
+            html += `
+            <div class="progress">
+              <span class="skill">${findSkill.name}<i class="val">${findSkill.level}%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="${findSkill.level}" aria-valuemin="0" aria-valuemax="${findSkill.level}"></div>
+              </div>
+            </div>`;
+          }
+        });
+        el.innerHTML = html;
+      });
+    }
+    // Projects
+    /*<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+      <div class="portfolio-wrap">
+        <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="" />
+        <div class="portfolio-links">
+          <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1">
+            <i class="bx bx-plus"></i>
+          </a>
+          <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+        </div>
+      </div>
+    </div>*/
+    const projectsEl = select("#portfolio .portfolio-container");
+    if (projectsEl) {
+      const { projects } = resumeInfo;
+      projectsEl.innerHTML = "";
+      projects.forEach((item) => {
+        projectsEl.innerHTML += `
+        <div class="col-lg-4 col-md-6 portfolio-item filter-${item.filter}">
+          <div class="portfolio-wrap">
+            <img src="${item.cover}" class="img-fluid" alt="" />
+            <div class="portfolio-links">
+              <a href="${item.cover}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="${item.name}">
+                <i class="bx bx-plus"></i>
+              </a>
+              <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+        </div>`;
+      });
+    }
+  })();
+
+  //? ---------------------------- END -------------------------
+})();
